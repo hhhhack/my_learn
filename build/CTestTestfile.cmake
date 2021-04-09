41dd1e44381a3cb236dd6f633cc27172bd6c6b84
hhhhack <1791743233@qq.com>
2021-04-09 07:35:14 -0700

    删除make文件

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cy/code/my_learn/alg
-# Build directory: /home/cy/code/my_learn/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
