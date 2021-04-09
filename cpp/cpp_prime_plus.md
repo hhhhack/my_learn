5d1a30ebd8a38dd789207622de798c0f8fb7d4cd
hhhhack <1791743233@qq.com>
2021-04-09 07:05:25 -0700

    之前没存的

--- cpp/cpp_prime_plus.md
+++ cpp/cpp_prime_plus.md
@@ -1,34 +1,47 @@
 <!--
- * 
- * 　　┏┓　　　┏┓+ +
- * 　┏┛┻━━━┛┻┓ + +
- * 　┃　　　　　　　┃ 　
- * 　┃　　　━　　　┃ ++ + + +
- *  ████━████ ┃+
- * 　┃　　　　　　　┃ +
- * 　┃　　　┻　　　┃
- * 　┃　　　　　　　┃ + +
- * 　┗━┓　　　┏━┛
- * 　　　┃　　　┃　　　　　　　　　　　
- * 　　　┃　　　┃ + + + +
- * 　　　┃　　　┃
- * 　　　┃　　　┃ +  神兽保佑
- * 　　　┃　　　┃    代码无bug　　
- * 　　　┃　　　┃　　+　　　　　　　　　
- * 　　　┃　 　　┗━━━┓ + +
- * 　　　┃ 　　　　　　　┣┓
- * 　　　┃ 　　　　　　　┏┛
- * 　　　┗┓┓┏━┳┓┏┛ + + + +
- * 　　　　┃┫┫　┃┫┫
- * 　　　　┗┻┛　┗┻┛+ + + +
- * 
- * 
- * @Author: hhhhack
- * @Date: 2021-04-01 17:29:20
- * @LastEditTime: 2021-04-01 17:29:22
- * @LastEditors: hhhhack
- * @Description: 
- * @FilePath: /code/my_learn/cpp/cpp.md
- * 
- -->
+ * @Author: your name
+ * @Date: 2021-04-01 10:15:44
+ * @LastEditTime: 2021-04-07 08:14:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /my_learn/cpp/cpp_prime_plus.md
+-->
 
+# 变量 #
+## 变量类型 ##
+- ### 整型
+  - short 至少16位
+  - int 至少与short长
+  - long 至少32位且，至少一个int
+  - long long 至少一个64位，且至少一个long长
+  > limits.h包含了各种类型的限制
+
+  > 初始化方式c++11之后支持{}
+  - unsigned类型
+  > cout控制符dec, hex, oct
+- ### 字符型
+  - char
+  - unsigned char
+  - wchar_t
+    > L''表示wchar_t，使用wcin,wcout输入输出
+  - char16_t, char32_t
+    > 可以表示unicode字符集u'', u'\uxxxx'
+- ### bool类型
+
+- ### const 限定符
+- ### 浮点数
+  - 书写方式, xxx.yyy 或者 xe[+/-]y
+  - flout 至少32位
+  - double 至少48位，至少和flout一样
+  - long double 至少和double一样
+- ### 算数运算符
+  - 运算符优先级
+  - 除法分支
+  - 类型转换
+    > 初始化或赋值时进行转换
+    
+    > 表达式包含不同类型
+
+    > 传参时
+- ### 复合类型
+  - 数组array
--- cpp/value.cpp
+++ cpp/value.cpp
@@ -1,60 +1,53 @@
 /*
- * 
- * 　　┏┓　　　┏┓+ +
- * 　┏┛┻━━━┛┻┓ + +
- * 　┃　　　　　　　┃ 　
- * 　┃　　　━　　　┃ ++ + + +
- *  ████━████ ┃+
- * 　┃　　　　　　　┃ +
- * 　┃　　　┻　　　┃
- * 　┃　　　　　　　┃ + +
- * 　┗━┓　　　┏━┛
- * 　　　┃　　　┃　　　　　　　　　　　
- * 　　　┃　　　┃ + + + +
- * 　　　┃　　　┃
- * 　　　┃　　　┃ +  神兽保佑
- * 　　　┃　　　┃    代码无bug　　
- * 　　　┃　　　┃　　+　　　　　　　　　
- * 　　　┃　 　　┗━━━┓ + +
- * 　　　┃ 　　　　　　　┣┓
- * 　　　┃ 　　　　　　　┏┛
- * 　　　┗┓┓┏━┳┓┏┛ + + + +
- * 　　　　┃┫┫　┃┫┫
- * 　　　　┗┻┛　┗┻┛+ + + +
- * 
- * 
- * @Author: hhhhack
- * @Date: 2021-04-01 19:25:35
- * @LastEditTime: 2021-04-01 20:49:59
- * @LastEditors: hhhhack
- * @Description: 
- * @FilePath: /code/my_learn/cpp/value.cpp
- * 
+ * @Author: your name
+ * @Date: 2021-04-07 08:12:27
+ * @LastEditTime: 2021-04-07 08:14:58
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /my_learn_1/cpp/value.cpp
  */
-
 #include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
 using namespace std;
 
-#define ValueSize(type, v) wcout << "size of type " << #type << " is " << sizeof(v) << " " << v << endl;
+const int ONESEC = 1000000;
 
-int main()
-{
-    short v1 = 0;
-    int v2 = 0;
-    long v3 = 0;
-    long long v4 = 0;
-    char v6 = '0';
-    wchar_t *v7 = L"我";
-    char16_t v8 = u'啊';
-    char32_t v9 = u'啊';
-    setlocale(LC_ALL, "chs");
-    ValueSize(short, v1);
-    ValueSize(int, v2);
-    ValueSize(long, v3);
-    ValueSize(long_long, v4);
-    ValueSize(char, v6);
-    ValueSize(wchar_t, v7);
-    ValueSize(char16_t, v8);
-    ValueSize(char32_t, v9);
+#define cout_value(type, v) cout << "size of type " << #type << \
+" is " << sizeof(v) << " value is " << v << endl;
+
+int main(){
+    char val_1 = 'v';
+    short val_2 = 1;
+    int val_3 = 2;
+    long val_4 = 043;
+    long long val_5 = 0x14;
+    wchar_t val_6 = '\a';
+    unsigned short val_7 = USHRT_MAX;
+    bool val_8 = true;
+    float val_9 = 54.5455;
+    double val_10 = 3.43e-5;
+    auto val_11 = INT_MAX + val_1;
+
+    cout_value(char, val_1);
+    cout <<"val_1 字面常量 是" << int(val_1) << " " <<' ';
+    cout.put(val_1);
+    cout << endl;
+    cout_value(short, val_2);
+    cout_value(int, val_3);
+    cout << oct;
+    cout_value(long, val_4);
+    cout << hex;
+    cout_value(long_long, val_5);
+    cout_value(wchar_t, val_6);
+    cout_value(u_short, val_7);
+    cout_value(bool, val_8);
+    cout_value(float, val_9);
+    cout_value(double, val_10);
+    cout << dec;
+    cout_value(int+int, long(long(INT_MAX) + INT_MAX));
+    cout_value(auto, val_11);
+
     return 0;
 }
